3d25e21b57b8e96f69ccb5c074f9d195a8016198
SatyakiB7076 <satyakit1998@gmail.com>
2025-06-18 22:29:08 +0530

    add backend routes to the application

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express, { json, urlencoded } from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnect from "./config/dbConnect.js";
+import authRoutes from "./routes/authRoutes.js"
+
+dotenv.config();
+dbConnect();
+const app = express();
+
+//middlewares
+const corsOption={
+    origin:"http://localhost:3001",
+    credentials: true,
+}
+app.use(json({limit:"100mb"}));
+app.use(urlencoded({limit:"100mb",extended:true}));
+app.use(cors(corsOption));
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 60000 * 60
+    }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
+app.use("/api/auth",authRoutes)
+//listen app
+const port =process.env.PORT || 7002;
+app.listen(port,()=>{
+    console.log(`Server is listening to port ${port}`);
+    
+})
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,21 @@
+import {Router} from "express";
+import passport from "passport";
+import {register,reset2FA,verify2FA,login,logout,setup2FA,authStatus} from "../controllers/authController.js"
+
+const router=Router();
+
+//registration Route
+router.post("/register",register);
+//Login Route
+router.post("/login",login);
+//Auth Status Route
+router.post("/status",authStatus);
+//Logout Route
+router.post("/logout",logout);
+//2fa setup
+router.post("/2fa/setup",setup2FA);
+//2fa verify
+router.post("/2fa/verify",verify2FA);
+//reset
+router.post("/2fa/reset",reset2FA);
+export default router;
