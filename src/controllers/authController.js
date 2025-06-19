b25f92e6193d9a1942d81d017e2e76de74e3a906
SatyakiB7076 <satyakit1998@gmail.com>
2025-06-20 01:39:09 +0530

    add 2fa routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
+import speakeasy from "speakeasy";
+import qrCode from "qrcode";
+import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -28,7 +31,7 @@ export const login = async (req, res) => {
 export const authStatus = async (req, res) => {
   if (req.user) {
     res.status(200).json({
-      message: "User logged in successfully",
+      message: "User authentication successfull",
       username: req.user.username,
       isMfaActive: req.user.isMfaActive,
     });
@@ -60,6 +63,61 @@ export const logout = async (req, res) => {
   });
 };
 
-export const verify2FA = async () => {};
-export const reset2FA = async () => {};
-export const setup2FA = async () => {};
+export const verify2FA = async (req, res) => {
+  const { token } = req.body;
+  const user = req.user;
+  const verified = speakeasy.totp.verify({
+    secret: user.twoFactorSecret,
+    encoding: "base32",
+    token,
+  });
+  //if the user is 2fa then only providing the jwt token for further api access
+  if (verified) {
+    const jwtToken = jwt.sign(
+      { username: user.username.User },
+      process.env.JWT_SECRET,
+      { expiresIn: "1hr" }
+    );
+    res.status(200).json({
+      message: "2fa successfull",
+      token: jwtToken,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid 2fa token" });
+  }
+};
+export const reset2FA = async (req, res) => {
+  try {
+    const user = req.user;
+    user.twoFactorSecret = "";
+    user.isMfaActive = false;
+    await user.save();
+    res.status(200).json({ message: "2fa reset successfull" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+export const setup2FA = async (req, res) => {
+  try {
+    const user = req.user;
+    let secret = speakeasy.generateSecret();
+    console.log("value of the secret object ", secret);
+
+    user.twoFactorSecret = secret.base32;
+    user.isMfaActive = true;
+    user.save();
+    const url = speakeasy.otpauthURL({
+      secret: secret.base32,
+      label: `${req.user.username}`,
+      issuer: "www.bhattacharjeeSatyaki.com",
+      encoding: "base32",
+    });
+    const qrImageUrl = await qrCode.toDataURL(url);
+    res.status(200).json({
+      qrCode: qrImageUrl,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Error setting up 2fa" });
+  }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,10 +12,25 @@ router.post("/login",passport.authenticate("local"),login);
 router.get("/status",authStatus);
 //Logout Route
 router.post("/logout",logout);
-//2fa setup
-router.post("/2fa/setup",setup2FA);
+
+
+//2fa setup is for authorized users only so adding middleware here 
+router.post("/2fa/setup",(req,res,next)=>{
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message:"Unauthorized user"})
+},setup2FA);
+
+
 //2fa verify
-router.post("/2fa/verify",verify2FA);
+router.post("/2fa/verify",(req,res,next)=>{
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message:"Unauthorized user"})
+},verify2FA);
+
+
 //reset
-router.post("/2fa/reset",reset2FA);
+router.post("/2fa/reset",(req,res,next)=>{
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message:"Unauthorized user"})
+},reset2FA);
 export default router;
